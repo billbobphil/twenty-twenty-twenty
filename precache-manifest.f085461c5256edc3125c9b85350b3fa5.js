e72a242bf576e4810e063d919627de03267b8cab
Trevor <37590467+billbobphil@users.noreply.github.com>
2019-10-07 20:26:52 -0600

    Updates

---
+++ precache-manifest.f085461c5256edc3125c9b85350b3fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fcdc186ed5bb1512a8a46b40a2cfc2",
+    "url": "/twenty-twenty-twenty/index.html"
+  },
+  {
+    "revision": "6d9f88cf53a73930dfa2",
+    "url": "/twenty-twenty-twenty/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "71a898df52f5a0e6042e",
+    "url": "/twenty-twenty-twenty/static/js/2.bd12f266.chunk.js"
+  },
+  {
+    "revision": "6d9f88cf53a73930dfa2",
+    "url": "/twenty-twenty-twenty/static/js/main.d99f96ef.chunk.js"
+  },
+  {
+    "revision": "8c445037bde47491100a",
+    "url": "/twenty-twenty-twenty/static/js/runtime-main.7036be6c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/twenty-twenty-twenty/static/media/logo.25bf045c.svg"
+  }
+]);
